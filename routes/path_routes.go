7fbdd24d908e20af0220cacab11138a343f94518
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-20 17:11:42 +0700

    add routes to generate token

--- routes/path_routes.go
+++ routes/path_routes.go
@@ -1,10 +1,13 @@
 package routes
 
 import (
-	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/twistycs/pokemon-go-backend/middlewares"
+	"github.com/twistycs/pokemon-go-backend/services"
 )
 
 func SetUpRoutes() *gin.Engine {
@@ -15,14 +18,31 @@ func SetUpRoutes() *gin.Engine {
 
 	r := gin.Default()
 
-	r.Use(func(c *gin.Context) {
-		fmt.Println("Have request coming")
-	})
+	login := r.Group("/v1/login")
+	{
+		login.POST("/", func(c *gin.Context) {
+			claims := services.TokenClaim{
+				"bar",
+				jwt.StandardClaims{
+					ExpiresAt: time.Now().Add(time.Minute * 1).Unix(),
+					Issuer:    "Wit",
+					IssuedAt:  time.Now().Unix(),
+				},
+			}
+			token, err := services.GenerateToken(claims)
+			if err != nil {
+				c.AbortWithStatusJSON(http.StatusInternalServerError, err.Error())
+			} else {
+				c.JSON(http.StatusOK, token)
+			}
+		})
+	}
 
 	user := r.Group("/v1/users")
+	user.Use(middlewares.AuthorizeBearer())
 	{
 		user.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusNotFound, "ID : Not Found")
+			c.JSON(http.StatusOK, "TEST")
 		})
 	}
 
