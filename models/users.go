01a3fcd857395cbc05c36ca23e8af512f828e0d4
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-20 14:41:16 +0700

    add user models

--- models/users.go
+++ models/users.go
@@ -1,4 +1,11 @@
 package models
 
+import "time"
+
 type User struct {
+	ID          uint      `gorm:"primaryKey;column:USER_ROW_ID"`
+	UserName    string    `gorm:"column:USERNAME;not null"`
+	Email       string    `gorm:"column:EMAIL;not null"`
+	CreatedDate time.Time `gorm:"column:CREATED_DATE"`
+	UpdatedDate time.Time `gorm:"column:UPDATED_DATE"`
 }
