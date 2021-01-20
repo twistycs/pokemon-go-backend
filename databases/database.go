6b27fe70e0ac6b295cecf5369ff30fa784237583
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-20 14:41:09 +0700

    add db config

---
+++ databases/database.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+type DBConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	DBName   string
+}
+
+//SetUpDb Database
+func SetUpDb() *DBConfig {
+	viper.SetConfigFile("./dbconfig.yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Can not read config file.")
+	}
+	var configDBs DBConfig
+	viper.Unmarshal(&configDBs)
+	return &configDBs
+}
+
+//ReadConfigDBFile print
+func (db *DBConfig) ReadConfigDBFile() string {
+	fmt.Printf("%+v", db)
+	return fmt.Sprintf(db.User + ":" + db.Password + "@tcp(" + db.Host + ")/" + db.DBName + "?charset=utf8&parseTime=True&loc=Local")
+}
