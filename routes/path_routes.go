1eb145336bc2f7e3cf522316ee05717e45929ac2
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-20 18:01:51 +0700

    add path routes

--- routes/path_routes.go
+++ routes/path_routes.go
@@ -2,40 +2,28 @@ package routes
 
 import (
 	"net/http"
-	"time"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/twistycs/pokemon-go-backend/controller"
+	database "github.com/twistycs/pokemon-go-backend/databases"
 	"github.com/twistycs/pokemon-go-backend/middlewares"
+	"github.com/twistycs/pokemon-go-backend/repositories"
 	"github.com/twistycs/pokemon-go-backend/services"
 )
 
 func SetUpRoutes() *gin.Engine {
 
-	// userRepo := repositories.RepositoriesConstruct(database.DB)
-	// userService := services.NewService(userRepo)
+	userRepo := repositories.UserRepositories(database.DB)
+	userService := services.NewUserService(userRepo)
 	// userController := controller.UserControllerInit(userService)
+
+	logInController := controller.LogInConstructor(userService)
 
 	r := gin.Default()
 
 	login := r.Group("/v1/login")
 	{
-		login.POST("/", func(c *gin.Context) {
-			claims := services.TokenClaim{
-				"bar",
-				jwt.StandardClaims{
-					ExpiresAt: time.Now().Add(time.Minute * 1).Unix(),
-					Issuer:    "Wit",
-					IssuedAt:  time.Now().Unix(),
-				},
-			}
-			token, err := services.GenerateToken(claims)
-			if err != nil {
-				c.AbortWithStatusJSON(http.StatusInternalServerError, err.Error())
-			} else {
-				c.JSON(http.StatusOK, token)
-			}
-		})
+		login.POST("/", logInController.LogInController)
 	}
 
 	user := r.Group("/v1/users")
