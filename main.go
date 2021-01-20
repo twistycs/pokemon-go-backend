9267e220388e2d3765cfdf7da7b8d9d15bc28bf7
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-20 14:41:36 +0700

    setup main

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	database "github.com/twistycs/pokemon-go-backend/databases"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var err error
 
 func main() {
-	fmt.Println("hello")
+	databaseInfo := database.SetUpDb()
+	database.DB, err = gorm.Open(mysql.Open(databaseInfo.ReadConfigDBFile()), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Connect database error : " + err.Error())
+	}
+
+	// database.DB.AutoMigrate(&models.Product{}, &models.Order{}, &models.OrderDetail{}, &models.User{}, &models.Branch{},
+	// 	&models.Stock{})
+
+	closeConnectionDB, _ := database.DB.DB()
+	defer fmt.Println("Close Connection DB !!!")
+	defer closeConnectionDB.Close()
+
+	// server := routes.SetUpRoutes()
+	// server.Run(":9999")
+
 }
