0fa36afe993a25579b2a2b2281e62996c33aac0f
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-20 18:01:09 +0700

    user repository

---
+++ repositories/user_repository.go
@@ -0,0 +1,50 @@
+package repositories
+
+import (
+	"github.com/twistycs/pokemon-go-backend/imp"
+	"github.com/twistycs/pokemon-go-backend/models"
+	"gorm.io/gorm"
+)
+
+type userRepository struct {
+	connect *gorm.DB
+}
+
+func UserRepositories(connect *gorm.DB) imp.UserRepository {
+	return &userRepository{connect}
+}
+
+func (repo *userRepository) GetAllUser(u *[]models.User) (err error) {
+	if err = repo.connect.Find(u).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo *userRepository) GetUserByUserName(u *models.User, userId string) (err error) {
+	if err = repo.connect.Where("username = ?", userId).Find(u).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo *userRepository) InsertUser(u *models.User) (err error) {
+	if err = repo.connect.Create(u).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo *userRepository) UpdateUser(u *models.User) (err error) {
+	if err = repo.connect.Save(u).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo *userRepository) DeleteUserById(id string) (err error) {
+	if err = repo.connect.Delete(&models.User{}, id).Error; err != nil {
+		return err
+	}
+	return nil
+}
