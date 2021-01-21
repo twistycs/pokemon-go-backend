d11a711ee442b48236a4778769cc17f368e84121
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-21 16:54:09 +0700

    add password field

--- models/users.go
+++ models/users.go
@@ -3,8 +3,9 @@ package models
 import "time"
 
 type User struct {
-	ID          uint      `gorm:"primaryKey;column:USER_ROW_ID"`
+	ID          uint      `gorm:"primaryKey;column:USER_ROW_ID"  json:"-"`
 	UserName    string    `gorm:"column:USERNAME;not null"`
+	Password    string    `gorm:"column:PASSWORD;not null" json:"-"`
 	Email       string    `gorm:"column:EMAIL;not null"`
 	CreatedDate time.Time `gorm:"column:CREATED_DATE"`
 	UpdatedDate time.Time `gorm:"column:UPDATED_DATE"`
