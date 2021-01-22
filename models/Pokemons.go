d8240baceb6bc32104c92363c82a94a319af532c
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-22 18:04:48 +0700

    remove ID json

--- models/Pokemons.go
+++ models/Pokemons.go
@@ -1,7 +1,7 @@
 package models
 
 type Pokemon struct {
-	ID            uint   `gorm:"primaryKey;column:POKEMON_ROW_ID"  json:"-"`
+	ID            uint   `gorm:"primaryKey;column:POKEMON_ROW_ID"`
 	Name          string `gorm:"column:NAME;not null"`
 	ImageUrl      string `gorm:"column:IMAGE_URL;not null"`
 	ImageUrlHiRes string `gorm:"column:IMAGE_URL_HI_RES;not null"`
